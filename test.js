97893a90ae660eec414a32dacb50fbe960bae183
Manthan Thakur <rockstarmanthanthakur@gmail.com>
2024-11-06 18:40:32 +0530

    latest

--- test.js
+++ test.js
@@ -8,53 +8,49 @@
 //             abc[j] = arr;
 //         }
 //     }
- 
+
 // }
 
 // console.log({abc})
 
-
-
 // let test = abc.split("")
 // const map = new Map()
 // for(let str of test){
-    //   if(map.has(str)){
-        //     map.set(str, map.get(str) + 1)
-        //   }else{
-            //   map.set(str , 1)
-            //   }
-            // }
-            
-            
-            // let mostO=0
-            // let strng = ""
-            // for(let str of test){
-                //    if(map.get(str) > mostO ){
-                    //     mostO = map.get(str) 
-                    //     strng = str
-                    //    }
-                    // }
-                    
-                    
-                    // console.log({mostO, strng})
-                    // function test(abc){
-                    //     let charCount={}
-                    //     for(let char of abc){
-                    //         if(charCount[char]){
-                    //             charCount[char]++
-                    //         }else{
-                    //             charCount[char]=1
-                    //         }
-                    //     }
-                    //     return charCount
-                    // }
-                    // let abc  = "helloworld"
+//   if(map.has(str)){
+//     map.set(str, map.get(str) + 1)
+//   }else{
+//   map.set(str , 1)
+//   }
+// }
 
-                    // console.log(test(abc));
+// let mostO=0
+// let strng = ""
+// for(let str of test){
+//    if(map.get(str) > mostO ){
+//     mostO = map.get(str)
+//     strng = str
+//    }
+// }
+
+// console.log({mostO, strng})
+// function test(abc){
+//     let charCount={}
+//     for(let char of abc){
+//         if(charCount[char]){
+//             charCount[char]++
+//         }else{
+//             charCount[char]=1
+//         }
+//     }
+//     return charCount
+// }
+// let abc  = "helloworld"
+
+// console.log(test(abc));
 
 // const a = [1,2,5,7,9,5]
 // // const a = [3,4,6,7,8,4,2,1]
-// const target = 7 
+// const target = 7
 
 // let b = [...a]
 // const sort = b.sort((a,b) => a-b)
@@ -70,13 +66,12 @@
 //         }else{
 //          break
 //         }
-    
+
 //     }
 // }
 
 // const a = [1,5,3,6,7]
 // const b =[4,3,5,2,4,2]
-
 
 // for(let str of b){
 //     if(a.indexOf(str) === -1){
@@ -84,18 +79,37 @@
 //     }
 // }
 
-
 // console.log(a)
 
-
 // setTimeout(() => {
-//    console.log("haello") 
+//    console.log("haello")
 // }, 1000, 'asdm');
 
-const obj = {
-    a :10
-}
+// const obj = {
+//     a :10
+// }
 
-Object.freeze(obj)
-obj.a = 20
-console.log(obj.age)
+// Object.freeze(obj)
+// obj.a = 20
+// console.log(obj.age)
+
+// const a = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+// console.log(flatFunction(a));
+
+// function flatFunction(arr) {
+//   let stack = [...arr];
+//   let result = [];
+
+//   while (stack.length) {
+//     const next = stack.pop();
+//     console.log({ next });
+//     if (Array.isArray(next)) {
+//       stack.push(...next); // Spread elements back to stack
+//       console.log({ stack });
+//     } else {
+//       result.push(next);
+//       console.log({ result });
+//     }
+//   }
+//   return result.reverse();
+// }
